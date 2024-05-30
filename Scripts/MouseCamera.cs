a5548c450c520ee8250c645dc322842f42e165a9
Maguire <136644201+LeopardDrager@users.noreply.github.com>
2024-05-30 10:46:47 -0500

    Added Playercam, MouseCamera, and MoveCamera.
    Revised Player.cs to be reliant on new input rather than legacy input, and added newly created other files, listed in the title.

---
+++ Scripts/MouseCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCamera : MonoBehaviour
+{
+
+       public Vector2 turn;
+       public float sensitivity = .5f;
+
+    void Start (){
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+ 
+     void Update()
+    {
+        turn.x += Input.GetAxis("Mouse X") * sensitivity;
+        turn.y += Input.GetAxis("Mouse Y") * sensitivity;
+        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+    }
+}
